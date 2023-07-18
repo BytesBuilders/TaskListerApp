dec1d89eda5bcbe45bf79f08ff5c5135d5333c5e
David Suárez <suarezds@javeriana.edu.co>
2023-07-18 15:55:05 -0500

    sign in / sign up

---
+++ Mobile/tasklister/lib/screens/auth/login_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:tasklister/screens/auth/signup_page.dart';
+
+import '../model/welcome_page.dart';
+
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      print('Email: $_email');
+      print('Contraseña: $_password');
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WelcomePage()),
+      );
+    }
+  }
+
+  void _goToSignUpPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio de sesión'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa tu email';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value!;
+                },
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, ingresa tu contraseña';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value!;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: Text('Iniciar sesión'),
+              ),
+              SizedBox(height: 8.0),
+              GestureDetector(
+                onTap: _goToSignUpPage,
+                child: Text(
+                  '¿No tienes una cuenta? Crea una aquí',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Mobile/tasklister/lib/screens/auth/signup_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crear cuenta'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '¡Crea una nueva cuenta!',
+              style: TextStyle(fontSize: 24.0),
+            ),
+            // Aquí puedes agregar los campos y lógica para el registro de cuenta
+          ],
+        ),
+      ),
+    );
+  }
+}
